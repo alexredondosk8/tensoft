120a8164dc14503197af09de1ab3f5de02720c22
ALEXANDER REDONDO <alexredondosk8@gmail.com>
2018-05-11 00:27:33 -0500

    se actualiza el arcivo gitignore para retirar las rutas que deben ser ignoradas

--- tensoft/RegPropietarios/models.py
+++ tensoft/RegPropietarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Gestión de clientes (Dueños de casa, Apartamento o Locales)
+# - Registrar clientes (los clientes deben poder poder crear su propia cuenta
+#   donde el login debe ser el correo electronico y debe tener validación Captcha
+#   Captcha).
+# - Actualizar datos del clientes
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellidos
