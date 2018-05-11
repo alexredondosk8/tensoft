1206aef34617ec32d433e32eb4a396a3a69393ce
ALEXANDER REDONDO <alexredondosk8@gmail.com>
2018-05-10 22:46:00 -0500

    se creo app RegPropietarios

--- tensoft/tensoft/tenant_urls.py
+++ tensoft/tensoft/tenant_urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 #from mensajes.views import MensajeCreateView
 
 urlpatterns = [
+    url(r'^registrar-propietarios/', include('RegPropietarios.urls')),
     url(r'^registrar-mensaje/', admin.site.urls),
 ]
