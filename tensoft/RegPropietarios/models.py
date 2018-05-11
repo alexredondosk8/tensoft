57b0180b270ee67946aca3dd0b00342f0c6d05a3
ALEXANDER REDONDO <alexredondosk8@gmail.com>
2018-05-11 01:49:26 -0500

    Se crea modelo de Usuario

--- tensoft/RegPropietarios/models.py
+++ tensoft/RegPropietarios/models.py
@@ -9,6 +9,3 @@ from django.db import models
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nombre + " " + self.apellidos
--- tensoft/tensoft/tenant_urls.py
+++ tensoft/tensoft/tenant_urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from RegPropietarios.views import *
 #from mensajes.views import MensajeCreateView
 
 urlpatterns = [
-    url(r'^registrar-propietarios/', include('RegPropietarios.urls')),
+    url(r'^registrar/$', ClientePropietario.as_view(), name="registrar-cliente"),
     url(r'^registrar-mensaje/', admin.site.urls),
 ]
