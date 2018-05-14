2806523fdc0fcb7079a1af59b502fde4dfba6689
ALEXANDER REDONDO <alexredondosk8@gmail.com>
2018-05-14 17:57:13 -0500

    Cambios Usuario, registrar, listar

--- tensoft/RegUsuarios/models.py
+++ tensoft/RegUsuarios/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Gestión de clientes (Dueños de casa, Apartamento o Locales)
 # - Registrar clientes (los clientes deben poder poder crear su propia cuenta
@@ -9,6 +11,12 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
-    correo = models.CharField(max_length=254, primary_key=True)
+    cedula = models.CharField(max_length=20, primary_key=True)
+    correo = models.EmailField(max_length=254)
     telefono = models.CharField(max_length=7)
     celular = models.CharField(max_length=10)
+    usuario = models.ForeignKey(User, null=True)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellidos
--- tensoft/RegUsuarios/views.py
+++ tensoft/RegUsuarios/views.py
@@ -14,17 +14,20 @@ from .models import Usuario
 from .forms import *
 # from .utils import *
 
-class ClienteUsuario(TemplateView):
+class Inicio(TemplateView):
+    template_name = 'app/index.html'
+
+class UsuarioCreateView(TemplateView):
     template_name = "RegUsuarios/cliente_form.html"
 
     def get_context_data(self, **kwargs):
-        context = super(ClienteUsuario, self).get_context_data(**kwargs)
+        context = super(UsuarioCreateView, self).get_context_data(**kwargs)
         context['form'] = FormRegistroCliente
 
         return context
 
     def post(self, request, *args, **kwargs):
-        context = super(ClienteUsuario, self).get_context_data(**kwargs)
+        context = super(UsuarioCreateView, self).get_context_data(**kwargs)
         print(request.POST)
 
         form_cliente = FormRegistroCliente(request.POST)
@@ -33,21 +36,86 @@ class ClienteUsuario(TemplateView):
             datos = form_cliente.cleaned_data
             nombre = datos['nombre']
             apellidos = datos['apellidos']
+            cedula = datos['cedula']
             correo = datos['correo']
             telefono = datos['telefono']
             celular = datos['celular']
 
-# se definen los datos
+            # VALIDA QUE EL CORREO NO ESTÉ REGISTRADO EN LA BD
+            try:
+                usuario = User.objects.get(username=correo)
+                context['form'] = form_cliente
+                context['existe'] = "Ya existe una cuenta vinculada con el correo" + correo + "."
+                return render(request, self.template_name, context)
 
-        cliente_registrado = Usuario(
-            nombre=nombre,
-            apellidos=apellidos,
-            correo=correo,
-            telefono=telefono,
-            celular=celular
-        )
+            except:
+                # se definen los datos
+                cliente_registrado = Usuario(
+                    nombre=nombre,
+                    apellidos=apellidos,
+                    cedula=cedula,
+                    correo=correo,
+                    telefono=telefono,
+                    celular=celular
+                )
+                cliente_registrado.save()
+                request.session['correo'] = correo
 
-        cliente_registrado.save()
-        request.session['correo'] = correo
+                return HttpResponseRedirect('/cuenta/registrar/usuario')
+        else:
+            context['form'] = form_cliente
+            return render(request, self.template_name, context)
 
-        return HttpResponseRedirect('/cuenta/registrar/usuario')
+class UsuarioClienteCreateView(TemplateView):
+    model = User
+    success_url = "/cuenta/login"
+    template_name = "RegUsuarios/user_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UsuarioClienteCreateView, self).get_context_data(**kwargs)
+
+        if self.request.session.get('correo'):
+            cliente = Usuario.objects.get(cedula=self.request.session.get('correo'))
+            context['cliente'] = cliente
+
+        else:
+            raise PermissionDenied
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = super(UsuarioClienteCreateView, self).get_context_data(**kwargs)
+        if request.POST['password'] and request.POST['password2']:
+            if request.POST['password'] == request.POST['password2']:
+                password = request.POST['password']
+                correo = cliente.correo
+                nuevo_usuario = User.objects.create_user(username=correo, password=password)
+                nuevo_usuario.save()
+                cliente.usuario = nuevo_usuario
+                cliente.save()
+
+                try:
+                    grupo = Group.objects.get(name='usuario-inmobiliaria')
+                    grupo.user_set.add(nuevo_usuario)
+                except:
+                    grupo = Group()
+                    grupo.name = 'usuario-inmobiliaria'
+                    grupo.save()
+                    grupo.user_set.add(nuevo_usuario)
+
+                del request.session['correo']
+                request.session['nuevo-registro'] = "Usted se ha registrado exitosamente. Por favor inicie sesión para continuar"
+
+            else:
+                cliente = Cliente.objects.get(cedula=self.request.session.get('cedula'))
+                context['cliente'] = cliente
+                context['no_match'] = "Las contraseñas no coinciden"
+                return render(request, self.template_name, context)
+
+            return redirect("/cuenta/login/", request=request)
+
+class Login(TemplateView):
+    template_name = "login.html"
+    
+class CuentaUsuario(TemplateView):
+    template_name = "RegUsuarios/cuenta_cliente.html"
