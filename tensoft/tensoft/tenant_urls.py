739b97b5673e6e6f241f2d2e1a0552504406b860
Nelson <nelson.herrera@correounivalle.edu.co>
2018-05-14 16:16:40 -0500

    Se agrega app propietario, registrar propietario de una inmobiliaria y se corrige en inmobiliaria_tenant.views dos puntos por un igual, actualizado gitignore

--- tensoft/tensoft/tenant_urls.py
+++ tensoft/tensoft/tenant_urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from inmuebles.views import *
+from propietarios.views import *
 
 
 url_inmuebles = [
     url(r'^registrar$', InmueblesCreateView.as_view(), name="registrar-inmueble"),
     url(r'^registrar/(?P<id_inmueble>[\w.@+-]+)/fotos/$', AgregarFotosInmueble.as_view(), name="registrar-fotos-inmueble"),
 ]
+url_propietario = [
+    url(r'^registrar$', PropietarioCreateView.as_view(), name="registrar-propietario")
+]
+
 urlpatterns = [
     url(r'^inmuebles/', include(url_inmuebles)),
+    url(r'^propietarios/', include(url_propietario)),
 ]
