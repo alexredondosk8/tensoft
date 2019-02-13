dc7b5857341ab4e6a10210c227be4b81130ecff3
alexredondosk8 <alexander.redondo@correounivalle.edu.co>
2019-02-12 20:40:12 -0500

    df

--- tensoft/tensoft/settings.py
+++ tensoft/tensoft/settings.py
@@ -218,7 +218,7 @@ USE_THOUSAND_SEPARATOR = False
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
