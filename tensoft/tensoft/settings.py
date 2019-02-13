3a3ebcb8073289d4e4063d44e448e6672f2d07bc
alexredondosk8 <alexander.redondo@correounivalle.edu.co>
2019-02-12 20:24:32 -0500

    ctivate MEDIA...

--- tensoft/tensoft/settings.py
+++ tensoft/tensoft/settings.py
@@ -224,5 +224,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
