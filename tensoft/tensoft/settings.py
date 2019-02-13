1e5b8655646d49046aa4c2f9fbc049cbd12e2dfe
alexredondosk8 <alexander.redondo@correounivalle.edu.co>
2019-02-12 19:29:23 -0500

    staticfiles_dirs fue modificado

--- tensoft/tensoft/settings.py
+++ tensoft/tensoft/settings.py
@@ -215,10 +215,10 @@ USE_THOUSAND_SEPARATOR = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    ("/home/alexredondo/tensoft/tensoft/tensoft/static"),
 ]
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
