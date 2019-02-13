d8550f4ad9b8de65f38d562c412d371c1168bcdc
alexredondosk8 <alexander.redondo@correounivalle.edu.co>
2019-02-12 20:02:28 -0500

    deative STATICFILES_DIRS y active STATIC_ROOT

--- tensoft/tensoft/settings.py
+++ tensoft/tensoft/settings.py
@@ -217,12 +217,11 @@ USE_THOUSAND_SEPARATOR = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/home/alexredondo/tensoft/tensoft/tensoft/static',
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
