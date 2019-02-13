373aad942e377dfba16aef126b5bb0855d6f7e64
alexredondosk8 <alexander.redondo@correounivalle.edu.co>
2019-02-12 20:43:53 -0500

    a

--- tensoft/tensoft/settings.py
+++ tensoft/tensoft/settings.py
@@ -219,6 +219,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    "/home/alexredondo/tensoft/tensoft/tensoft/static"
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
